513197e30dcecafb7ce81680f8163e68cc34ee96
Alfonso Fajardo <faj.alf@gmail.com>
2018-11-06 13:35:40 -0600

    t.test files

---
+++ t_test_andyfield.R
@@ -0,0 +1,35 @@
+#### learning to do t test with R. Andy Field data
+library(tidyverse)
+library(pander)
+spiderwide <- read.csv("SpiderWide.csv",sep = "\t") %>%
+mutate(pMean = (picture +real)/2 ) %>%
+mutate(grandMean = mean(c(picture,real))) %>%
+mutate(adj = grandMean - pMean)%>%
+mutate (picture_adj = picture + adj)%>%
+mutate(real_adj  = real+adj) %>%
+  select(picture_adj,real_adj)
+
+attach(spiderwide)
+
+spiderlong <- read.csv("spiderlong.csv",sep="\t")
+
+summary(lm(Anxiety ~ Group, data = spiderlong))
+
+### Calculte t test manually
+
+x1 <- mean(spiderlong$Anxiety [spiderlong$Group=="Real Spider"])
+x2 <- mean(spiderlong$Anxiety[spiderlong$Group=="Picture"])
+sd1 <- sd(spiderlong$Anxiety [spiderlong$Group=="Real Spider"])
+sd2 <- sd(spiderlong$Anxiety[spiderlong$Group=="Picture"])
+n1 <- length(spiderlong$Group[spiderlong$Group =="Real Spider"])
+n2 <- length(spiderlong$Group[spiderlong$Group =="Picture"])
+
+
+ttestfromMeans<-function(x1, x2, sd1, sd2, n1, n2)
+{
+  df<-n1 + n2 - 2
+  poolvar<-(((n1-1)*sd1^2)+((n2-1)*sd2^2))/df
+  t<-(x1-x2)/sqrt(poolvar*((1/n1)+(1/n2)))
+  sig<-2*(1-(pt(abs(t),df)))
+  paste("t(df = ", df, ") = ", t, ", p = ", sig, sep = "")
+}
