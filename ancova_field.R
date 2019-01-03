67b75611040ae7d430b72164ccfce2d1c01cb84e
Alfonso Fajardo <faj.alf@gmail.com>
2019-01-02 22:07:04 -0600

    mooved files

--- ancova_field.R
+++
@@ -1,9 +0,0 @@
-## ANCOVA --- Andy Field Book
-library(multcomp)
-library(compute.es)
-library(effects)
-library(ggplot2)
-library(pastecs)
-library(WRS2)
-
-viagraData <- read.csv("ViagraCovariate.csv",sep="\t")
---
+++ introduction_to_meta-analysis/course_notes.md
@@ -0,0 +1,16 @@
+# **Introduction to Systematic reviews and Meta-analysis**
+
+**¿ what is a systematic review?**
+
+how do you know which medical treatment is more effective?
+
+
+
+
+
+
+
+
+
+
+
